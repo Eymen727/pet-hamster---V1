5c96d3eb98bc336562d27cbba88296c35c72dfcc
Eymen KÖSE <159542670+Eymen727@users.noreply.github.com>
2026-04-01 15:29:14 +0300

    Update pxt.json, main.blocks, main.ts, pxt.json

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
-input.onButtonPressed(Button.B, function () {
-    manyetik = input.magneticForce(Dimension.X)
-    led.plotBarGraph(
-    manyetik,
-    1023,
-    true
-    )
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.happy.playUntilDone()
+    basic.showIcon(IconNames.Asleep)
 })
 input.onGesture(Gesture.ScreenDown, function () {
     basic.showLeds(`
@@ -15,10 +12,16 @@ input.onGesture(Gesture.ScreenDown, function () {
         . . . . .
         `)
 })
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-    soundExpression.happy.playUntilDone()
-    basic.showIcon(IconNames.Asleep)
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.B, function () {
+    manyetik = input.magneticForce(Dimension.X)
+    led.plotBarGraph(
+    manyetik,
+    1023,
+    true
+    )
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Sad)
@@ -29,15 +32,6 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showIcon(IconNames.Happy)
     soundExpression.happy.playUntilDone()
     basic.showIcon(IconNames.Asleep)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        . # # # .
-        . . . . .
-        `)
 })
 let manyetik = 0
 soundExpression.hello.playUntilDone()
