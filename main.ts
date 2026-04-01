c22e59722e25cd76204570cc870b3b8ada7107d8
Eymen KÖSE <159542670+Eymen727@users.noreply.github.com>
2026-04-01 15:18:12 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-    soundExpression.happy.playUntilDone()
-    basic.showIcon(IconNames.Asleep)
+input.onButtonPressed(Button.B, function () {
+    manyetik = input.magneticForce(Dimension.X)
+    led.plotBarGraph(
+    manyetik,
+    1023,
+    true
+    )
 })
 input.onGesture(Gesture.ScreenDown, function () {
     basic.showLeds(`
@@ -12,22 +15,10 @@ input.onGesture(Gesture.ScreenDown, function () {
         . . . . .
         `)
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        . # # # .
-        . . . . .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    manyetik = input.magneticForce(Dimension.X)
-    led.plotBarGraph(
-    manyetik,
-    1023,
-    true
-    )
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.happy.playUntilDone()
+    basic.showIcon(IconNames.Asleep)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Sad)
@@ -38,6 +29,15 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showIcon(IconNames.Happy)
     soundExpression.happy.playUntilDone()
     basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
 })
 let manyetik = 0
 soundExpression.hello.playUntilDone()
