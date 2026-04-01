b40da233377500b44cc2163299a0d3dadef5eda5
Eymen KÖSE <159542670+Eymen727@users.noreply.github.com>
2026-04-01 15:48:02 +0300

    Update pxt.json, main.blocks, main.ts, pxt.json

--- main.ts
+++ main.ts
@@ -1,16 +1,52 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-    soundExpression.happy.playUntilDone()
-    basic.showIcon(IconNames.Asleep)
-})
-input.onGesture(Gesture.ScreenDown, function () {
+input.onGesture(Gesture.EightG, function () {
     basic.showLeds(`
-        . . . . .
-        # # . # #
+        # . . . #
+        . # . # .
         . . . . .
         . # # # .
-        . . . . .
+        # . . . #
         `)
+    music.play(music.createSoundExpression(WaveShape.Noise, 5000, 5000, 206, 206, 5000, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . . . #
+        # . . . .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showIcon(IconNames.Asleep)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        # . . . .
+        . # # # .
+        `)
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.A, function () {
+    soundExpression.happy.playUntilDone()
+    basic.showIcon(IconNames.Heart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(0.1)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Asleep)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showArrow(ArrowNames.North)
